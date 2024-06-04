1d2c00edbea9b926c565c075676fb4713f1ee095
Lucas <lucasbaroquello@hotmail.com>
2024-06-03 23:43:09 -0300

    feat: vercel.json routes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import App from './app.js';
 import router from './router.js';
-import './styles/main.css';
-import './styles/homePage.css';
-import './styles/checkoutPage.css';
-import './styles/cardProduct.css';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const root = document.getElementById('app');
